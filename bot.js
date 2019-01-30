dde84be6ca01d39c59ad64af62eb199ae8d24831
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-01-30 21:00:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,27 +27,5 @@ client.user.setGame(``,'https://www.twitch.tv/tarikrs');                        
 
 
 
-const adminprefix = "$";
-const devs = ['496597218195800084','438371243801051141'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'بلاي')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق  إلى **`)
-} else 
-  if (message.content.startsWith(adminprefix + 'نيم')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم  إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'افتار')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة `);
-      } else     
-if (message.content.startsWith(adminprefix + 'ستريم')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");//wennnn
-    message.channel.sendMessage(`**تم تغيير تويتش  إلى  ${argresult}**`)
-}
-});؛
+
+
