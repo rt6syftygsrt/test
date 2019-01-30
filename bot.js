b02cdd0cfac30b72ca2f80afa026fdd895267c3b
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-01-30 21:11:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,6 @@ client.user.setGame(``,'https://www.twitch.tv/tarikrs');                        
 
 
 
-
 const adminprefix = "!";
 const devs = ['496597218195800084','438371243801051141'];
 client.on('message', message => {
@@ -36,19 +35,19 @@ client.on('message', message => {
     
 if (message.content.startsWith(adminprefix + 'بلاي')) {
   client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق الحساب إلى **`)
 } else 
   if (message.content.startsWith(adminprefix + 'نيم')) {
 client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم الحساب إلى`)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
   if (message.content.startsWith(adminprefix + 'افتار')) {
 client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة الحساب `);
       } else     
 if (message.content.startsWith(adminprefix + 'ستريم')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk");//wennnn
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+    message.channel.sendMessage(`**تم تغيير تويتش الحساب  إلى  ${argresult}**`)
 }
 });
